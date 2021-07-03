1d193f7c48f61dc866e2ba3e72db2563e30e2c33
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2021-07-03 21:20:26 +0100

    D

--- Class/D-functions/exercise5.js
+++ Class/D-functions/exercise5.js
@@ -4,8 +4,8 @@
 // It should return an output, which is the value of the two inputs added together
 
 function addTwoNumbers (x , y) {
- console.log (x + y);
+ return x + y;
 }
 
 let result = addTwoNumbers(3, 5);
-
+console.log(result);
