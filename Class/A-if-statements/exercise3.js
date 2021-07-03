535000d5f120253a0d5bbcc18eda20025854948d
sPuoja <84133321+sPuoja@users.noreply.github.com>
2021-07-03 10:48:48 +0100

    Done Exercise A

--- Class/A-if-statements/exercise3.js
+++ Class/A-if-statements/exercise3.js
@@ -4,7 +4,7 @@ let weather = "Rainy";
 let clothesMessage = "Wear Sunglasses";
 
 // Complete the "if" statement so it is true if the value of weather is "Rainy"
-if(){
+if(weather === "rainy"){
     clothesMessage = "Take an umbrella";
 }
 
