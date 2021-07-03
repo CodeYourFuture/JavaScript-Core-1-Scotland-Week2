1f5090a31c5f5a852e503cbe63ba989e8558425c
Megumi Yanaka <margaret_mori0102@yahoo.com>
2021-07-03 20:16:51 +0100

    Finished by file"Class"

--- Class/C-logical-operators/exercise2.js
+++ Class/C-logical-operators/exercise2.js
@@ -1,15 +1,15 @@
 
 // We would like to check if myAge is between 13 and 18
 
-let myAge = 21;
+let myAge = 25;
 
 let message = "Not a teenager";
 
 // Write an "if" statement below, and change the value of "message" if the age is a teenager
+if(myAge >= 13 && myAge <= 18){
+    message = "A teenager";
+}
+
 // Check if myAge is greater than or equal to 13, AND less than or equal to 18
-
-
-
-
-
 console.log(message);
+
