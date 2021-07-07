f4514a4531cbc708cfbd2a88a64c74ecc4147638
andrejskalnins <84182469+andrejskalnins@users.noreply.github.com>
2021-07-07 12:02:28 +0100

    finished exercises in D-functions folder

--- Class/D-functions/exercise1.js
+++ Class/D-functions/exercise1.js
@@ -1,14 +1,12 @@
-
 // Here is a simple function with no inputs and no output
-function sayHello(){
-    console.log("Hello");
+function sayHello() {
+  console.log("Hello");
 }
 
 // This is how we call the function
 // sayHello();
 
 // Add code below so we print "Hello" 3 times
-
-
-
-
+sayHello();
+sayHello();
+sayHello();
--- Class/D-functions/exercise4.js
+++ Class/D-functions/exercise4.js
@@ -1,11 +1,11 @@
-
-// This function has one input: inputNumber, and an output 
-function doubleNumber(inputNumber){
-    return inputNumber * 2;
+// This function has one input: inputNumber, and an output
+function doubleNumber(inputNumber) {
+  return inputNumber * 2;
 }
 
-function tripleNumber(inputNumber){
-    // Write code here so this function gives the correct result
+function tripleNumber(inputNumber) {
+  // Write code here so this function gives the correct result
+  return inputNumber * 3;
 }
 
 let x = 10;
