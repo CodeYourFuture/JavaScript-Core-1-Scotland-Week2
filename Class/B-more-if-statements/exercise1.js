46b0eecc59f61d21c692b31cc508628ece326986
andrejskalnins <84182469+andrejskalnins@users.noreply.github.com>
2021-07-07 10:58:58 +0100

    finished exercises in B folder

--- Class/B-more-if-statements/exercise1.js
+++ Class/B-more-if-statements/exercise1.js
@@ -1,4 +1,3 @@
-
 let today = "Saturday";
 
 let classDay = "Saturday";
@@ -6,8 +5,8 @@ let classDay = "Saturday";
 let message = "No class today";
 
 // Write your own "if" statement below to set the value of message to "CYF class today!" if today is a class day
-
-
-
+if (today === classDay) {
+  message = "CYF class today!";
+}
 
 console.log(message);
