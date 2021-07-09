bdba0f8c554260966ed422d24cf13333e9bf581c
naviclatech20 <84205622+naviclatech20@users.noreply.github.com>
2021-07-09 17:30:21 +0100

    I done exercise1.js from Function
     I answered to question 1 and 2 and returned the age in months.

--- Coursework/2-Functions/exercise1.js
+++ Coursework/2-Functions/exercise1.js
@@ -3,13 +3,21 @@ What is the value we expect to see in the console?
 
 Why do we see "undefined"?
 
+
 ONLY CHANGE the code of calculateAgeInMonths function - 
 please fix this bug so we see the result is "360"
 */
 
-function calculateAgeInMonths(ageInYears){
-    let ageInMonths = ageInYears * 12;
-    // remember we have to return the value!
+let question1 = "We expected to see the value of '30'";
+console.log(question1);
+let question2 =
+  "we see undefined because we never declared the return and what to return inside the function. ";
+console.log(question2);
+
+function calculateAgeInMonths(ageInYears) {
+  let ageInMonths = ageInYears * 12;
+  // remember we have to return the value!
+  return ageInMonths;
 }
 
 let myAge = 30;
