1f7ede942dbf2969e29f8eaa9114412eb4f7a578
naviclatech20 <84205622+naviclatech20@users.noreply.github.com>
2021-07-09 16:45:03 +0100

    Exercises4.js is 1-If-Else folder is completed.

--- Coursework/1-If-Else/exercise4.js
+++ Coursework/1-If-Else/exercise4.js
@@ -4,9 +4,17 @@ let redScore = 3;
 
 let blueScore = 5;
 
-// Write an "if/else" statement below to print either - 
+// Write an "if/else" statement below to print either -
 // "Red has won!"
 // "Blue has won!"
 // "It is a draw"
 
+if (redScore > blueScore) {
+  message = " Red has Won!";
+} else if (redScore < blueScore) {
+  message = "Blue has won!";
+} else {
+  message = "it is a draw";
+}
 
+console.log(message);
