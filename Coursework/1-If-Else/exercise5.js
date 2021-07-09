7565cad8d69ec46141cc1c1a64096e4e3f010649
naviclatech20 <84205622+naviclatech20@users.noreply.github.com>
2021-07-09 17:14:19 +0100

    Execise4.js from 1-If-Else's exercises completed.
     I converted negative number to positive number and around it to two decimal.

--- Coursework/1-If-Else/exercise5.js
+++ Coursework/1-If-Else/exercise5.js
@@ -2,12 +2,25 @@
 
 let myMoney = 1.66;
 
-let sandwichCost = 2.50;
+let sandwichCost = 2.5;
 
-// Write an "if" statement below to print either - 
+// Write an "if" statement below to print either -
 // "You can afford the sandwich :-)" or
 // "You don't have enough money :-("
-
-
+if (sandwichCost <= myMoney) {
+  message = "You can afford the sandwich :-)";
+} else {
+  message = "You don't have enough money :-(";
+}
+console.log(message);
 
 // For extra credit, can you tell me how much change I get?
+
+let additionalOrChange = myMoney - sandwichCost;
+noChange = additionalOrChange.toFixed(2);
+let reverseNoChange = noChange * -1;
+console.log(reverseNoChange);
+
+console.log(
+  " No change instead you will need extra money equal to " + reverseNoChange
+);
