0f48ecca303aa01eee2788dedd671f63a7c289fa
naviclatech20 <84205622+naviclatech20@users.noreply.github.com>
2021-07-09 14:45:04 +0100

    I completed execise1 on if- else.

--- Coursework/1-If-Else/exercise1.js
+++ Coursework/1-If-Else/exercise1.js
@@ -1,4 +1,3 @@
-
 let today = "Friday";
 
 let message = "No";
@@ -6,7 +5,8 @@ let message = "No";
 // Write an "if" statement to set the value of message to "Yes"
 // if today is Friday
 
-
-
+if (today === "Friday") {
+  message = "YES";
+}
 
 console.log("Is today Friday? " + message);
