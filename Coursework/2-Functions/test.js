9be5396731c7c974172cb0c938f07e90daf05d08
naviclatech20 <84205622+naviclatech20@users.noreply.github.com>
2021-07-09 18:28:36 +0100

    I made a function with name and parameters
     I changed the line to call a function so I can have the same result.

---
+++ Coursework/2-Functions/test.js
@@ -0,0 +1,18 @@
+let firstName1 = "Katrina";
+let surname1 = "Lake";
+// Change the line below to call your function!
+let greeting1 = "Hello " + firstName1 + " " + surname1;
+
+let firstName2 = "Eric";
+let surname2 = "Yuan";
+// Change the line below to call your function!
+let greeting2 = "Hello " + firstName2 + " " + surname2;
+
+let firstName3 = "Jeff";
+let surname3 = "Bezos";
+// Change the line below to call your function!
+let greeting3 = "Hello " + firstName3 + " " + surname3;
+
+console.log(greeting1);
+console.log(greeting2);
+console.log(greeting3);
