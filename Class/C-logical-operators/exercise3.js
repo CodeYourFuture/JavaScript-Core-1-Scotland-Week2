faea81deeaeaff1917b51e0d0c6dc45d25d0f24c
Benjamin-GitHub <benjaminmhrdd@gmail.com>
2021-07-09 22:32:55 +0100

    C-logical-operators
    Exercises completed

--- Class/C-logical-operators/exercise3.js
+++ Class/C-logical-operators/exercise3.js
@@ -1,10 +1,10 @@
 
-let theTime = 11;
+let theTime = 9;
 
 let theDay = "Saturday"
 
 // Complete the "if" statement to check if theDay is Saturday, 
 // AND the time is equal to or greater than 10
-if(){
+if(theTime >= 10 && theDay === "Saturday"){
     console.log("Class has started");
 }
