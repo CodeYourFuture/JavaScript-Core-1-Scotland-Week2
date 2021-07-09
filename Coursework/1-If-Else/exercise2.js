e6878137a69d22026eca8042b62034c33467c335
naviclatech20 <84205622+naviclatech20@users.noreply.github.com>
2021-07-09 14:51:48 +0100

    exercise2.js in 1-If-Else completed.

--- Coursework/1-If-Else/exercise2.js
+++ Coursework/1-If-Else/exercise2.js
@@ -1,4 +1,3 @@
-
 let today = "Wednesday";
 
 let message = "Yes";
@@ -6,7 +5,8 @@ let message = "Yes";
 // Write an "if" statement to set the value of message to "No"
 // if today is NOT Tuesday
 
-
-
+if (today !== "Tuesday") {
+  message = "No";
+}
 
 console.log("Is today Tuesday? " + message);
