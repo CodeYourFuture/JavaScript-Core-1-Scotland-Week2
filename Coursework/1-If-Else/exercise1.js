5f42dbb281f60ee6037b9bbd8cf2714274014a9f
Tandy-Nicole <84181728+Tandy-Nicole@users.noreply.github.com>
2021-09-01 20:41:40 +0100

    completed If-Else exercise

--- Coursework/1-If-Else/exercise1.js
+++ Coursework/1-If-Else/exercise1.js
@@ -6,7 +6,9 @@ let message = "No";
 // Write an "if" statement to set the value of message to "Yes"
 // if today is Friday
 
+if (today === Friday) {
 
+    console.log("Is today Friday? " + Yes);
 
+}
 
-console.log("Is today Friday? " + message);
